389afa11ce04296848b9a92f8ae745c9eb52c240
mzhaonanq <1002604003@qq.com>
2020-06-28 22:20:03 +0800

    仿写

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require("url");
 var port = process.argv[2];
 
 if (!port) {
-  console.log("请指定端口号好不啦？\nnode server.js 8888 这样不会吗？");
+  console.log("端口去哪里啦？\n输入 node server.js 8888 才OK啦！");
   process.exit(1);
 }
 
@@ -21,12 +21,20 @@ var server = http.createServer(function (request, response) {
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
+  console.log("有个大聪明发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
 
   if (path === "/") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`二哈`);
+    response.write(`
+    <!DOCTYPE html>
+    <head>
+    <link rel="stylesheet" href="/x">
+    </head>
+    <body>
+    <h1>Hello World</h1>
+    </body>
+    `);
     response.end();
   } else if (path === "/x") {
     response.statusCode = 200;
@@ -36,7 +44,7 @@ var server = http.createServer(function (request, response) {
   } else {
     response.statusCode = 404;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`你输入的路径不存在对应的内容`);
+    response.write(`你访问的页面不存在`);
     response.end();
   }
 
@@ -45,8 +53,5 @@ var server = http.createServer(function (request, response) {
 
 server.listen(port);
 console.log(
-  "监听 " +
-    port +
-    " 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:" +
-    port
+  "监听 " + port + " 成功\n请在托马斯回旋时打开 http://localhost:" + port
 );
